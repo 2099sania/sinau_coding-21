e7d447dfe5b0410019a6ae5adc8e9dd738286ab5
2099sania <102446423+2099sania@users.noreply.github.com>
2022-08-01 14:50:41 +0700

    Add files via upload
    upload file java untuk tugas 2 java

---
+++ tugas2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      for (int x=1; x <= 20; x++){
+        if(x % 2 != 0){
+          System.out.println(x + "Adalah Bilangan Prima");
+        }
+      }
+      
+  }
+}
